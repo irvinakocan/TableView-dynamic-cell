404a669de6d4f3ab8045e68d4b72451b6c415843
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2024-02-22 15:05:58 +0100

    Initial Commit

---
+++ TableView dynamic cell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView dynamic cell
+//
+//  Created by Macbook Air 2017 on 22. 2. 2024..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
